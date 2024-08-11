333e65b2fe6500956ec95de64839761a88e151c5
Swiftboat2 <90280978+Swiftboat2@users.noreply.github.com>
2024-08-11 06:57:04 -0300

    refactoring y testeo

--- hello_test.go
+++ hello_test.go
@@ -7,21 +7,22 @@ func TestHello(t *testing.T) {
 
 		got := Hello("Chris")
 		want := "Hola Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 
 	})
 
 	t.Run("deci 'Hola mundo' cuando se entregue un string vacio", func(t *testing.T) {
 		got := Hello("")
 		want := "Hola Mundo"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+//en got, want string se puede poner una sola vez string al final
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 
 }
