3f426adec9b57348679c101c4e6f21bf70d8deb3
Swiftboat2 <90280978+Swiftboat2@users.noreply.github.com>
2024-08-12 10:26:54 -0300

    Iteration

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,15 +1,23 @@
-
 package integers
-import "testing"
 
-func TestAdder(t *testing.T){
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
 	expected := 4
 
 	assertSuma(t, sum, expected)
-	
+
 }
 
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+
+}
 
 func assertSuma(t testing.TB, sum, expected int) {
 	t.Helper()
