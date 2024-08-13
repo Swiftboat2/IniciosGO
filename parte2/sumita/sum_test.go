5eb578ebb4ae6ccc75089804be3f58dee9633a82
Swiftboat2 <90280978+Swiftboat2@users.noreply.github.com>
2024-08-13 11:35:27 -0300

    Suma final de los slices

--- parte2/sumita/sum_test.go
+++ parte2/sumita/sum_test.go
@@ -1,32 +1,60 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-t.Run("coleccion de 5 numeros", func(t *testing.T) {
-	numbers := []int{1, 2, 3, 4, 5}
+	t.Run("coleccion de 5 numeros", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Suma(numbers)
-	want := 15
+		got := Suma(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("colleccion de cualquier tamaño", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Suma(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+
+	})
+}
+
+func TestSumaTodo(t *testing.T) {
+	got := SumaTodo([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d want %d", got, want)
 	}
-})
+}
+func TestSumaTodoFinales(t *testing.T) {
+	t.Run("suma el final del array", func(t *testing.T) {
+		got := SumaTodoFinal([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
 
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 
-t.Run("colleccion de cualquier tamaño", func(t *testing.T) {
-	numbers := []int{1, 2, 3}
+	t.Run("suma slices vacias", func(t *testing.T) {
+		got := SumaTodoFinal([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
 
-	got:= Suma(numbers)
-	want := 6
-
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
-
-
-})
-
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 
 }
--- parte2/sumita/suma.go
+++ parte2/sumita/suma.go
@@ -7,3 +7,22 @@ func Suma(numeros []int) int {
 	}
 	return sum
 }
+func SumaTodo(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Suma(numbers))
+	}
+	return sums
+}
+func SumaTodoFinal(numbersToSum ...[]int) []int {
+	var sumas []int
+	for _, numbers := range numbersToSum {
+	if len(numbers) == 0{
+	sumas = append(sumas, 0)
+	}else{
+		tail := numbers[1:]
+		sumas = append(sumas, Suma(tail))
+		}
+	}
+	return sumas
+}
